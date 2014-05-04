f14a0f21a5a504f7d8dea4b39831520b8dfbcadb
Ky-Anh Huynh <kyanh@theslinux.org>
2014-05-04 21:09:50 +0700

    lib/homebrew: Homebrew support on Mac OS

---
+++ lib/homebrew.sh
@@ -0,0 +1,103 @@
+# Purpose: Homebrew support
+# Author : Anh K. Huynh
+# License: Fair license (http://www.opensource.org/licenses/fair)
+# Source : http://github.com/icy/pacapt/
+
+# Copyright (C) 2010 - 2014 Anh K. Huynh
+#
+# Usage of the works is permitted provided that this instrument is
+# retained with the works, so that any entity that uses the works is
+# notified of this instrument.
+#
+# DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+
+homebrew_Qi() {
+  brew info "$@"
+}
+
+homebrew_Ql() {
+  brew list "$@"
+}
+
+homebrew_Qo() {
+  local pkg prefix cellar
+
+  cd "$(dirname -- $(which "$@"))"
+  pkg="$(pwd -P)/$(basename -- "$@")"
+  prefix="$(brew --prefix)"
+  cellar="$(brew --cellar)"
+
+  for package in $cellar/*; do
+    files=(${package}/*/${pkg/#$prefix\//})
+    if [[ -e "${files[${#files[@]} - 1]}" ]]; then
+      echo "${package/#$cellar\//}"
+      break
+    fi
+  done
+}
+
+homebrew_Qc() {
+  brew log "$@"
+}
+
+homebrew_Qu() {
+  brew outdated | grep "$@"
+}
+
+homebrew_Q() {
+  if [[ "$_TOPT" == "" ]]; then
+    brew list | grep "$@"
+  else
+    _not_implemented
+  fi
+}
+
+homebrew_Rs() {
+  if [[ "$_TOPT" == "s" ]]; then
+    brew rm "$@"
+    brw rm $(join <(brew leaves) <(brew deps "$@"))
+  else
+    _not_implemented
+  fi
+}
+
+homebrew_R() {
+  brew remove "$@"
+}
+
+homebrew_Si() {
+  brew info "$@"
+}
+
+homebrew_Suy() {
+  brew update \
+  && brew upgrade "$@"
+}
+
+homebrew_Su() {
+  bre upgrade "$@"
+}
+
+homebrew_Sy() {
+  brew update "$@"
+}
+
+homebrew_Ss() {
+  brew search "$@"
+}
+
+homebrew_Sc() {
+  brew cleanup "$@"
+}
+
+homebrew_Scc() {
+  bre cleanup -s "$@"
+}
+
+homebrew_Sccc() {
+  rm -rf $(brew --cache)
+}
+
+homebrew_S() {
+  brew install $_TOPT "$@"
+}
