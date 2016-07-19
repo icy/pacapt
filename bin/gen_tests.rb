f816680f810fa7b8e940adbe67ec224f494a0245
Ky-Anh Huynh <kyanh@theslinux.org>
2016-07-19 14:13:39 +0700

    bin/gen_tests.rb: Write all logs to STDERR for future reference

--- bin/gen_tests.rb
+++ bin/gen_tests.rb
@@ -20,12 +20,13 @@ BEGIN {
   puts "N_TEST=0"
   puts "N_FAIL=0"
   puts ""
-  puts "_fail() { echo -e \"\\e[0;31mFail:\\e[0m $*\"; }" # red
-  puts "_erro() { echo -e \"\\e[0;31mErro:\\e[0m $*\"; }" # red
-  puts "_info() { echo -e \"\\e[0;36mInfo:\\e[0m $*\"; }" # cyan
-  puts "_pass() { echo -e \"\\e[0;36mPass:\\e[0m $*\"; }" # cyan
-  puts "_exec() { echo -e \"\\e[0;33mExec:\\e[0m $*\"; }" # yellow
-  puts "_warn() { echo -e \"\\e[0;33mFail:\\e[0m $*\"; }" # yellow
+  puts "_log()  { echo 1>&2; echo \":: $*\" 1>&2 ; }"
+  puts "_fail() { _log \"Fail $*\"; echo -e \"\\e[0;31mFail:\\e[0m $*\"; }" # red
+  puts "_erro() { _log \"Erro $*\"; echo -e \"\\e[0;31mErro:\\e[0m $*\"; }" # red
+  puts "_info() { _log \"Info $*\"; echo -e \"\\e[0;36mInfo:\\e[0m $*\"; }" # cyan
+  puts "_pass() { _log \"Pass $*\"; echo -e \"\\e[0;36mPass:\\e[0m $*\"; }" # cyan
+  puts "_exec() { _log \"Exec $*\"; echo -e \"\\e[0;33mExec:\\e[0m $*\"; }" # yellow
+  puts "_warn() { _log \"Warn $*\"; echo -e \"\\e[0;33mWarn:\\e[0m $*\"; }" # yellow
 }
 
 if gs = $_.match(/^# test\.in(.*)/)
@@ -50,14 +51,14 @@ if gs = $_.match(/^# test\.in(.*)/)
   when "clear" then
     "echo > $F_TMP"
   when /\A! (.+)\z/ then
-    "#{$1.strip} 2>>$F_TMP 1>&2"
+    "#{$1.strip}"
   else
-    "pacman #{cmd} 2>>$F_TMP 1>&2"
+    "pacman #{cmd}"
   end
 
   puts "if [[ -n \"${F_TMP:-}\" ]]; then"
-  puts "  _exec \"#{cmd}\""
-  puts "  #{cmd}"
+  puts "  _exec \"#{cmd} (log: $F_TMP)\""
+  puts "  { #{cmd} ; } 2>&1 | tee -a $F_TMP 1>&2"
   puts "fi"
 
 elsif gs = $_.match(/^# test\.ou(.*)/)
@@ -85,7 +86,7 @@ end
 
 END {
   puts "if [[ $N_FAIL -ge 1 ]]; then"
-  puts "  _warn \"$N_FAIL/$N_TEST test(s) failed.\""
+  puts "  _fail \"$N_FAIL/$N_TEST test(s) failed.\""
   puts "  exit 1"
   puts "else"
   puts "  _pass \"All $N_TEST tests(s) passed.\""
