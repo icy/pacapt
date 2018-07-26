c02702ae8d1192e02ba524b6e2534b5890b64228
Ky-Anh Huynh <kyanh@viettug.org>
2018-07-26 10:29:36 +0700

    lib/conda: initialize sample

---
+++ lib/conda.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Purpose : Conda support
+# Author  : FIXME
+# License : MIT
+# Date    : FIXME
+
+_conda_init() {
+  :
+}
+
+# FIXME: If `Qq` is defined instead of `Q`,
+# FIXME: user would get error `Q::q` is not valid.
+conda_Q() {
+  _warn "This is a sample implementation"
+}
+
+conda_Ss() {
+  _not_implemented
+}
